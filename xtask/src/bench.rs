7802975e8e79f6281d348186ea23103722a423f1
Jasper van den Bogart <jasper.opti@gmail.com>
2025-10-09 16:48:04 +0200

    Remove previously implemented TODO marker

--- xtask/src/bench.rs
+++ xtask/src/bench.rs
@@ -6,7 +6,6 @@ impl flags::Bench {
     pub(crate) fn run(self, sh: &Shell) -> anyhow::Result<()> {
         let Self {} = self;
 
-        // TODO store benchmark results in repo
         cmd!(sh, "cargo run --release --package salsita_test bench").run()?;
 
         Ok(())
