1646d4a2c63c3b7200a67a0c34b5b293e184e33f
Jasper van den Bogart <jasper.opti@gmail.com>
2025-10-17 12:11:05 +0200

    Store `Q::eval` as a type-erased thunk inside `MemoEntry`

--- salsita/src/lib.rs
+++ salsita/src/lib.rs
@@ -19,7 +19,7 @@ mod tests;
 #[derive(Debug, Default)]
 pub struct Db<M> {
     memo_index: MemoIndex,
-    memo_entries: MemoEntries,
+    memo_entries: MemoEntries<M>,
     active_queries: ActiveQueryStack,
     metrics: M,
 }
@@ -42,13 +42,14 @@ where
 }
 
 #[derive(Debug, Default)]
-struct MemoEntries {
-    entries: RefCell<Vec<RefCell<MemoEntry>>>,
+struct MemoEntries<M> {
+    entries: RefCell<Vec<RefCell<MemoEntry<M>>>>,
 }
 
 #[derive(Debug)]
-struct MemoEntry {
+struct MemoEntry<M> {
     deps: Vec<MemoId>,
+    eval: fn(&Db<M>, &dyn Any) -> MemoValueAny,
     value: Option<MemoValueAny>,
 }
 
@@ -109,11 +110,13 @@ where
                     .clone()
             })
         } else {
-            self.memo_entries
-                .entry_mut(memo_id, |entry| entry.deps.clear());
+            let eval = self.memo_entries.entry_mut(memo_id, |entry| {
+                entry.deps.clear();
+                entry.eval
+            });
             self.active_queries.push_query(memo_id);
             let eval_guard = self.metrics.enter_eval();
-            let out = Q::eval(self, args);
+            let out = eval(self, args).downcast::<Q>().clone();
             self.metrics.exit_eval(eval_guard);
             self.active_queries.pop_query();
             out
@@ -135,7 +138,7 @@ where
     where
         Q: Query,
     {
-        let id = self.memo_entries.alloc_entry();
+        let id = self.memo_entries.alloc_entry::<Q>();
         self.memo_index.insert_memo::<Q>(make_args(id), id);
         id
     }
@@ -189,23 +192,29 @@ impl PerQueryIndexAny {
     }
 }
 
-impl MemoEntries {
-    fn alloc_entry(&self) -> MemoId {
+impl<M> MemoEntries<M>
+where
+    M: Metrics,
+{
+    fn alloc_entry<Q>(&self) -> MemoId
+    where
+        Q: Query,
+    {
         let mut entries = self.entries.borrow_mut();
         let raw_id = RawId::new(entries.len());
-        entries.push(RefCell::new(MemoEntry::new()));
+        entries.push(RefCell::new(MemoEntry::new::<Q>()));
         MemoId::from(raw_id)
     }
 
-    fn entry_mut<T>(&self, id: MemoId, f: impl FnOnce(&mut MemoEntry) -> T) -> T {
+    fn entry_mut<T>(&self, id: MemoId, f: impl FnOnce(&mut MemoEntry<M>) -> T) -> T {
         f(&mut self.entry_cell(id).borrow_mut())
     }
 
-    fn entry<T>(&self, id: MemoId, f: impl FnOnce(&MemoEntry) -> T) -> T {
+    fn entry<T>(&self, id: MemoId, f: impl FnOnce(&MemoEntry<M>) -> T) -> T {
         f(&mut self.entry_cell(id).borrow())
     }
 
-    fn entry_cell(&self, id: MemoId) -> Ref<'_, RefCell<MemoEntry>> {
+    fn entry_cell(&self, id: MemoId) -> Ref<'_, RefCell<MemoEntry<M>>> {
         Ref::map(self.entries.borrow(), |entries| {
             entries
                 .get(id.idx())
@@ -214,11 +223,30 @@ impl MemoEntries {
     }
 }
 
-impl MemoEntry {
-    const fn new() -> Self {
-        Self {
+impl<M> MemoEntry<M>
+where
+    M: Metrics,
+{
+    const fn new<Q>() -> Self
+    where
+        Q: Query,
+    {
+        return Self {
             deps: Vec::new(),
+            eval: eval::<M, Q>,
             value: None,
+        };
+
+        fn eval<M, Q>(db: &Db<M>, args: &dyn Any) -> MemoValueAny
+        where
+            M: Metrics,
+            Q: Query,
+        {
+            let args = args
+                .downcast_ref()
+                .unwrap_or_else(|| panic!("type cast failed"));
+            let out = Q::eval(db, args);
+            MemoValueAny::new::<Q>(out)
         }
     }
 
