670381232402211c07fd2723cadb6600a63f9051
Jasper van den Bogart <jasper.opti@gmail.com>
2025-11-07 15:33:41 +0100

    Move update-memo-on-drop structs to a separate module

--- salsita/src/lib.rs
+++ salsita/src/lib.rs
@@ -12,6 +12,10 @@ use crate::registry::QueryRegistry;
 use crate::storage::DefaultStorage;
 use crate::storage::Downcast;
 use crate::storage::Storage;
+use crate::update::MemoUpdate;
+use crate::update::PendingChange;
+use crate::update::PendingCommit;
+use crate::update::QueryUpdate;
 use alloc::sync::Arc;
 use core::any::type_name;
 use core::cell::RefCell;
@@ -29,6 +33,7 @@ mod registry;
 pub mod storage;
 #[cfg(test)]
 mod tests;
+mod update;
 
 const INCONSISTENT_STATE: &str = "bug: database in inconsistent state due to panic";
 
@@ -82,44 +87,6 @@ where
     S: Storage,
 {
     deps: Vec<MemoId<S>>,
-}
-
-#[must_use]
-struct QueryUpdate<'snap, S, H>
-where
-    S: Storage,
-    H: event::Handler,
-{
-    snapshot: &'snap Snapshot<S, H>,
-    commit: PendingCommit<S>,
-}
-
-#[must_use]
-struct MemoUpdate<'memos, S>
-where
-    S: Storage,
-{
-    memos: &'memos Memos<S>,
-    commit: PendingCommit<S>,
-}
-
-#[must_use]
-struct PendingCommit<S>
-where
-    S: Storage,
-{
-    current_rev: Revision,
-    memo_id: MemoId<S>,
-    change: Option<PendingChange<S>>,
-}
-
-#[must_use]
-struct PendingChange<S>
-where
-    S: Storage,
-{
-    value_id: S::Id,
-    deps: Option<Vec<MemoId<S>>>,
 }
 
 impl<S, H> Db<S, H>
@@ -300,103 +267,6 @@ where
     fn default() -> Self {
         Self {
             deps: Vec::default(),
-        }
-    }
-}
-
-impl<'snap, S, H> QueryUpdate<'snap, S, H>
-where
-    S: Storage,
-    H: event::Handler,
-{
-    const fn new(snapshot: &'snap Snapshot<S, H>, commit: PendingCommit<S>) -> Self {
-        Self { snapshot, commit }
-    }
-}
-
-impl<S, H> Drop for QueryUpdate<'_, S, H>
-where
-    S: Storage,
-    H: event::Handler,
-{
-    fn drop(&mut self) {
-        let Self { snapshot, commit } = self;
-        let mut active_queries = snapshot.active_queries.borrow_mut();
-        let ActiveQueryStack(active_queries) = &mut *active_queries;
-        if let Some(change) = commit.change.as_mut()
-            && let Some(ActiveQuery { deps }) = active_queries.pop()
-        {
-            change.deps = Some(deps);
-        }
-        let _update = MemoUpdate::new(&snapshot.db.global.memos, commit.take());
-    }
-}
-
-impl<'memos, S> MemoUpdate<'memos, S>
-where
-    S: Storage,
-{
-    const fn new(memos: &'memos Memos<S>, commit: PendingCommit<S>) -> Self {
-        Self { memos, commit }
-    }
-}
-
-impl<S> Drop for MemoUpdate<'_, S>
-where
-    S: Storage,
-{
-    fn drop(&mut self) {
-        let Self {
-            memos,
-            commit:
-                PendingCommit {
-                    current_rev,
-                    memo_id,
-                    change,
-                },
-        } = self;
-        memos.memo_mut(*memo_id, |memo| {
-            memo.last_verified = *current_rev;
-            if let Some(PendingChange { deps, value_id }) = change.take() {
-                memo.value_id = Some(value_id);
-                memo.last_changed = *current_rev;
-                if let Some(deps) = deps {
-                    memo.deps = deps;
-                }
-            }
-        });
-    }
-}
-
-impl<S> PendingCommit<S>
-where
-    S: Storage,
-{
-    const fn new(current_rev: Revision, memo_id: MemoId<S>) -> Self {
-        Self {
-            current_rev,
-            memo_id,
-            change: None,
-        }
-    }
-
-    const fn take(&mut self) -> Self {
-        Self {
-            change: self.change.take(),
-            current_rev: self.current_rev,
-            memo_id: self.memo_id,
-        }
-    }
-}
-
-impl<S> PendingChange<S>
-where
-    S: Storage,
-{
-    const fn new(value_id: S::Id) -> Self {
-        Self {
-            value_id,
-            deps: None,
         }
     }
 }
